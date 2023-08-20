6770b96b237902776fb8be9662255cd7c599c08b
MaxRyazan <no4ta87@mail.ru>
2023-08-20 15:47:15 +0300

    add dist

---
+++ dist/assets/MainPage-db93a245.js
@@ -0,0 +1,7 @@
+import{_ as a,c as l,o as t,p as o,a as _,b as e}from"./index-ac489913.js";const c={},n=s=>(o("data-v-06e5d844"),s=s(),_(),s),i={class:"all_wrapper"},d=n(()=>e("span",null," Программа - блокнот. Создана с использованием: ",-1)),r=n(()=>e("ul",null,[e("li",null,"Vue3.js"),e("li",null,"Pinia"),e("li",null,"Vite"),e("li",null,"vue-router, vue-animations, vuelidate"),e("li",null,"Typescript"),e("li",null,"Scss"),e("li",null,"LocalStorage")],-1)),u=n(()=>e("pre",{style:{"font-family":"Fifaks, sans-serif"}},`            Дополнительная информация:
+            Users:
+            {
+                "username": "Max",
+                "password":"123456789"
+            }
+        `,-1)),p=[d,r,u];function f(s,h){return t(),l("div",i,p)}const m=a(c,[["render",f],["__scopeId","data-v-06e5d844"]]);export{m as default};

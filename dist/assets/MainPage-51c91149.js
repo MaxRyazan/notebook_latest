1461f4d2f87e6b0346303f77db2582b401170d64
MaxRyazan <no4ta87@mail.ru>
2023-09-09 12:11:12 +0300

    2.3  -добавил страницу просмотра одной заметки  - скорректировал компоненты Reusable-Input.vue, ReusableTextArea.vue

--- dist/assets/MainPage-51c91149.js
+++
@@ -1,7 +0,0 @@
-import{_ as a,c as d,o as t,a as e}from"./index-d52710c5.js";const s={},i={class:"all_wrapper"},o=e(`<span data-v-d552789d> Программа - блокнот. Создана с использованием: </span><ul data-v-d552789d><li data-v-d552789d>Vue3.js</li><li data-v-d552789d>Pinia</li><li data-v-d552789d>Vite</li><li data-v-d552789d>vue-router, vue-animations, vuelidate</li><li data-v-d552789d>Typescript</li><li data-v-d552789d>Scss</li><li data-v-d552789d>LocalStorage</li><li data-v-d552789d>Adaptive design</li></ul><pre style="font-family:Fifaks, sans-serif;" data-v-d552789d>            Дополнительная информация:
-            Users:
-            {
-                &quot;username&quot;: &quot;Max&quot;,
-                &quot;password&quot;:&quot;123456789&quot;
-            }
-        </pre>`,3),n=[o];function l(_,c){return t(),d("div",i,n)}const u=a(s,[["render",l],["__scopeId","data-v-d552789d"]]);export{u as default};
